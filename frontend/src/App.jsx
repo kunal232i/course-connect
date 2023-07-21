68f5896ec3e143f78578d8443ff747f8c66bb4be
kunal232i <kingawale23@gmail.com>
2023-07-21 01:18:50 +0000

    frontend : init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Landing from "./components/Landing";
+import CreateCourse from "./components/CreateCourse";
+import Signup from "./components/Register";
+import ShowCourses from "./components/ShowCourses";
+import Navbar from "./components/Navbar";
+import UpdateCourse from "./components/UpdateCourse";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/createCourse" element={<CreateCourse />} />
+        <Route path="/courses" element={<ShowCourses />} />
+        <Route path="/updateCourse/:courseId" element={<UpdateCourse />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
